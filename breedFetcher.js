24343b3eeeb64c4902354afd84398e30f2b0bae7
Ali <alihbouran@gmail.com>
2022-05-12 17:04:59 -0700

    added a test and index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
 const request = require('request');
 const args = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  if (error) {
-    return console.log("Failed to recieve request", error);
-  }
-  const data = JSON.parse(body);
-  if (data.length < 1) {
-    console.log("Requested breed is not found. Try another");
-  } else {
-    console.log(data[0].description);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+    if (data.length < 1) {
+      return callback("Requested breed doesn't exist", null);
+    }
+      
+    callback(null, data[0].description);
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
