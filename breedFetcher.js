033c5bf3e6614edd161af925ce5ccb919c507626
Ali <alihbouran@gmail.com>
2022-05-12 18:43:24 -0700

    Added and linted tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,16 @@
 const request = require('request');
-const args = process.argv[2];
 
 const fetchBreedDescription = function(breedName, callback) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
       return callback(error, null);
     }
 
     const data = JSON.parse(body);
     if (data.length < 1) {
-      return callback("Requested breed doesn't exist", null);
+      console.log("no breed");
+      return callback("Requested breed doesn't exist.", null);
     }
-      
     callback(null, data[0].description);
     
   });
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -4,23 +4,22 @@ const { assert } = require('chai');
 describe('fetchBreedDescription', () => {
   it ('returns a string description for a valid breed, via callback', (done) => {
     fetchBreedDescription('Siberian', (err, desc) => {
-      assert.equal(err, null)
+      assert.equal(err, null);
 
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
-      
       assert.equal(expectedDesc, desc.trim());
       
       done();
     })
-  })
+  });
 
   it ('returns an error in fetching details, via callback', (done) => {
     fetchBreedDescription('Wookiee', (err, desc) => {
-      assert.equal(err, null)
       
-      const expectedErr = "Error fetch details: Requested breed is not found. Try another."
+      const expectedErr = "Requested breed doesn't exist."
       
       assert.equal(err.trim(), expectedErr);
-    })
-  })
-})
+      done()
+    });
+  });
+});
